c09efb907d1806d5886bb4eb51b33f45a0684946
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-04-10 10:03:28 -0500

    Create area.py

---
+++ area.py
@@ -0,0 +1,15 @@
+from math import sqrt
+"""
+Entradas
+a-->float-->a
+b-->float
+c-->float
+Salidas
+area-->float
+"""
+a=2
+b=2
+c=2
+s=(a+b+c)/2
+area=sqrt((s*(s-a)*(s-b)*(s-c)))
+print("el area es: "+str(area))
