b13ee0c68e656b2aed5bab919a15cf37a34a6152
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-04-10 08:54:49 -0500

    Create promedio.py

---
+++ promedio.py
@@ -0,0 +1,13 @@
+"""
+Entrandas
+edad1--int-->edad1
+edad2--int-->edad2
+edad3--int-->edad3
+Salidas
+Promedioedad--float-->promedio
+"""
+edad1=int(input("digite edad1: "))#int
+edad2=int(input("digite edad2: "))#int
+edad3=int(input("digite edad3: "))#int
+promedio=(edad1+edad2+edad3)/3#float
+print("El promedio de edad es: "+str(promedio))#str
