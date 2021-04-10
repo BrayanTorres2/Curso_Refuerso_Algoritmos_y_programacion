721f33ccd9c4dad7dbd185936cff660f31ec08a5
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-04-10 09:41:16 -0500

    Add files via upload

---
+++ descuento.py
@@ -0,0 +1,9 @@
+'''
+Entradas
+compra total--->ct--->float
+Salidas
+totalapagar-->total--->float
+'''
+ct=float(input("digite compra total:  "))
+total=ct*0.15
+print("total a pagar con descuento: "+str(ct-total))
