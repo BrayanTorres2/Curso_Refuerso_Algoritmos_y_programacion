25c032069e0072943a0a8a5e1bf4982101cd41b4
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-04-29 12:41:59 -0500

    Create filter.py

---
+++ ProgramacionFuncional/filter.py
@@ -0,0 +1,9 @@
+#Funciones de orden Superior
+#funcion filter (funcion,lista)
+lista=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+def es_par(n):
+  if(n%2==0):
+    return True
+  else:
+    return False
+print(list(filter( es_par, lista)))   
